2e6ecad657878535a5a44338bbdfa13aa430b30a
Sangeet Paudel <stiletdurden252@gmail.com>
2024-01-14 02:04:35 +0545

    Add and Delete Items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Todo = () => {
+    const[data, setData] = useState('');
+    const[items, setItems] = useState([]);
+
+    const addItem = () =>{
+        if(!data){
+            window.alert('Write something to add!')
+        }else{
+            setItems([...items, data]);
+            setData('');
+        }
+        
+    }
+
+    const deleteItem = (id) =>{
+        const updatedItem = items.filter((elem, ind)=>{
+            return(
+                ind!==id 
+            )
+        });
+        setItems(updatedItem);
+    }
   return (
     <div className='main'>
         <div className="whole-thing">
             <div className="form">
-                <input type="text" placeholder='Add your tasks here' />
-                <button>Add</button>
+                <input type="text" placeholder='Write your tasks here' value={data} onChange={(e)=>setData(e.target.value)}/>
+                <button onClick={addItem}>Add</button>
             </div>
-            <div className="list">
-                <p>Shopping</p>
-            </div>
+            {items.map((elem, ind)=>{
+                return(
+                    <div className="list" key={ind}>
+                        <p>{elem}</p>
+                        <FontAwesomeIcon icon={faPenToSquare}/>
+                        <FontAwesomeIcon icon={faTrash} onClick={()=>deleteItem(ind)}/>
+                     </div>
+                )
+            })}
+            
+           
         </div>
     </div>
   )
